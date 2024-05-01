c12195cd4339b1c872c028783f1ba8b247acdd09
Kseniya <kseniya.kudelich@gmail.com>
2024-04-30 20:57:26 -0600

    Change Create Item modal styling

--- frontend/src/components/NewItemModal.tsx
+++ frontend/src/components/NewItemModal.tsx
@@ -38,42 +38,43 @@ interface NewItemModalProps {
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"> {/* This is the modal background */}
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white"> {/* This is the modal box */}
         <form onSubmit={handleSubmit}>
-          <input
+        <input
             type="text"
             name="name"
             value={newItemData.name}
             onChange={handleChange}
             placeholder="Name"
-            className="mb-2 p-1"
+            className="mb-2 p-2 border-2 border-gray-300 rounded-md w-full"
             required
-          />
-          <input
+            />
+            <input
             type="text"
             name="description"
             value={newItemData.description}
             onChange={handleChange}
             placeholder="Description"
-            className="mb-2 p-1"
+            className="mb-2 p-2 border-2 border-gray-300 rounded-md w-full"
             required
-          />
-          <input
+            />
+            <input
             type="text"
             name="serial"
             value={newItemData.serial}
             onChange={handleChange}
             placeholder="Serial Number"
-            className="mb-2 p-1"
+            className="mb-2 p-2 border-2 border-gray-300 rounded-md w-full"
             required
-          />
-          <input
+            />
+            <input
             type="number"
             name="quantity"
             value={newItemData.quantity}
             onChange={handleChange}
             placeholder="Quantity"
-            className="mb-2 p-1"
+            className="mb-2 p-2 border-2 border-gray-300 rounded-md w-full"
             required
-          />
+            />
+
           <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Item</button>
           <button type="button" onClick={() => onItemCreated()} className="ml-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Cancel</button>
         </form>
